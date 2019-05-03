f63bf637cd5e90dd4f5e5de125fc9700aea6f825
minhbth-1850 <tran.hoang.minhb@sun-asterisk@.com>
2019-05-03 13:42:54 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :load_user, except: %i(new index create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return @user if @user
-
-    flash[:danger] = t "message.nil_user"
-    redirect_to root_path
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.users.per_page
   end
+
+  def show; end
 
   def create
     @user = User.new user_params
@@ -22,10 +26,55 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "message.profile_update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "message.user_del"
+      redirect_to users_path
+    else
+      flash[:danger] = t "message.nil_user"
+      redirect_to root_path
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email,
       :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "message.login_plz"
+    redirect_to login_path
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return @user if @user
+
+    flash[:danger] = t "message.nil_user"
+    redirect_to root_path
   end
 end
--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class SessionsHelperTest < ActionView::TestCase
   def setup
     @user = users(:michael)
-    remember(@user)
+    remember @user
   end
 
   test "current_user returns right user when session is nil" do
