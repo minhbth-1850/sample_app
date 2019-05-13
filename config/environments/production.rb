25c198f9498818b365db9ed9467836de138f756c
minhbth-1850 <tran.hoang.minhb@sun-asterisk@.com>
2019-05-13 15:53:39 +0700

    production mailer, upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,14 @@ Rails.application.configure do
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "stark-ridge-35768.herokuapp.com"
+  host = "powerful-gorge-29164.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: "587",
     authentication: :plain,
-    user_name: ENV["sendgrid_username"],
-    password: ENV["sendgrid_password"],
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
     domain: ENV["domain"],
     enable_starttls_auto: true
   }
